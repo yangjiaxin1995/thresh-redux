74a2e8390d72d41290a200571262dc94fba2e427
Thresh <601426683@qq.com>
2022-07-20 22:18:19 +0800

    feat: 实现createStore基础

--- src/pages/ReduxPage.jsx
+++ src/pages/ReduxPage.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import store from '../store';
 
-const ReduxPage = () => {
-  return <div>ReduxPage</div>;
-};
+export default class ReduxPage extends Component {
+  componentDidMount() {
+    // 告诉redux，一旦state变化（一旦执行dispatch函数）,就执行的事件
+    this.unsubscribe = store.subscribe(() => {
+      this.forceUpdate();
+    });
+  }
 
-export default ReduxPage;
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  add = () => {
+    store.dispatch({ type: 'ADD' });
+  };
+
+  minus = () => {
+    // setTimeout(() => {
+    //   store.dispatch({ type: "MINUS" });
+    // }, 1000);
+
+    store.dispatch((dispatch) => {
+      setTimeout(() => {
+        dispatch({ type: 'MINUS' });
+      }, 1000);
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>ReduxPage</h3>
+        <p>{store.getState()}</p>
+        <button onClick={this.add}>add</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
+}
